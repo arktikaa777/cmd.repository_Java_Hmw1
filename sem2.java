53bef5da0b639fa7245d9ae5331b15f55fede691
Genya <arktikaa777@gmail.com>
2023-02-22 01:52:35 +1300

    add "Homework 4"

--- sem2.java
+++ sem2.java
@@ -1,6 +1,9 @@
 // алгоритм сортировки пузырьком числового массива, 
 //результат после каждой итерации запишите в лог-файл.
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -11,7 +14,7 @@ import java.util.logging.SimpleFormatter;
 
 public class sem2 {
    
-/*
+
     public static void main(String[] args) throws SecurityException, IOException {
         int[] mas = {15, 2, 8, 16, 7};
         boolean isSorted = false;
@@ -40,7 +43,8 @@ public class sem2 {
         logger.info("bubble sort algorithm");
     
     }
-    
+}    
+    /*
 // Дана строка sql-запроса "select * from students where ". 
 // Сформируйте часть WHERE этого запроса, используя StringBuilder.
 public static void main(String[] args) {
@@ -51,24 +55,23 @@ public static void main(String[] args) {
     obj.put("age",null);
     System.out.println(getQuery(obj));
 }
-public static String getQuery(Map<String, String> object)
-{
-    StringBuilder s = new StringBuilder();
-    for (Map.Entry<String,String> pair : object.entrySet())
-    {
-        if (pair.getValue() != null)
-        {
-            s.append(pair.getKey() +" = '" + pair.getValue()+"' and ");
+    public static String getQuery(Map<String, String> object) {
+        StringBuilder s = new StringBuilder();
+        for (Map.Entry<String,String> pair : object.entrySet()) {
+            if (pair.getValue() != null) {
+                s.append(pair.getKey() +" = '" + pair.getValue()+"' and ");
+            }
         }
+        s.delete(s.toString().length()-5,s.toString().length());
+        return s.toString();
     }
-    s.delete(s.toString().length()-5,s.toString().length());
-    return s.toString();
-}
   
 
-// task ***
-public static void main(String[] args) {
-    StringBuilder str = new StringBuilder("name Ivanov mark 5 subject Mathematics");
+    // task *** строку [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"} 
+    // переделать в -> Студент Иванов получил 5 по предмету Математика
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("name Ivanov mark 5 
+                                              subject Mathematics");
 
     // print string
     System.out.println("String = " + str.toString());
@@ -79,59 +82,60 @@ public static void main(String[] args) {
 
     // print string
     System.out.println("After Replace() String = " + strReturn.toString());
-}
+    }
 
 
 
-public static void main(String[] args) {
-    String master = "name Иванов oценка предмет Математика";
-    String target = "name";
-    String replacemeString = "student";
+    public static void main(String[] args) {
+        String master = "name Иванов oценка предмет Математика";
+        String target = "name";
+        String replacemeString = "student";
 
-    int startIndex = master.indexOf(target);
-    int stopIndex = startIndex + target.length();
+        int startIndex = master.indexOf(target);
+        int stopIndex = startIndex + target.length();
 
-    StringBuilder builder = new StringBuilder(master);
-    
-}
-}
-*/
+        StringBuilder builder = new StringBuilder(master);
+    }
 
 
-public static void main(String[] args) {
-    try {
-        File myObj = new File("Students.txt");
-        if (myObj.createNewFile()) {
-            System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } 
+
+
+
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("Students.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } 
         catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-}  
+    }  
 
-/*public static void modifyFile(String filePath, String oldString, String newString) {
-    File fileToBeModified = new File(filePath);
-    StringBuilder oldContent = new StringBuilder();
-    try (BufferedReader reader = new BufferedReader(new FileReader(fileToBeModified))) {
-        String line = reader.readLine();
-        while (line != null) {
-            oldContent.append(line).append(System.lineSeparator());
-            line = reader.readLine();
+    public static void modifyFile(String filePath, String oldString, String newString) {
+        File fileToBeModified = new File(filePath);
+        StringBuilder oldContent = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileToBeModified))) {
+            String line = reader.readLine();
+            while (line != null) {
+                oldContent.append(line).append(System.lineSeparator());
+                line = reader.readLine();
+            }
+            String content = oldContent.toString();
+            String newContent = content.replaceAll(oldString, newString);
+            try (FileWriter writer = new FileWriter(Students.txt)) {
+                writer.write(newContent);
+            }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
         }
-        String content = oldContent.toString();
-        String newContent = content.replaceAll(oldString, newString);
-        try (FileWriter writer = new FileWriter(Students.txt)) {
-            writer.write(newContent);
-        }
-    } 
-    catch (IOException e) {
-        e.printStackTrace();
-    }
-}  
+    }  
+}    
 */
 
-}
+
